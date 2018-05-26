6d68d9bc14b406fec41a672773f336e2d058cee0
Lugamo <bitlugamo@gmail.com>
2018-05-26 18:34:11 -0500

    Adding first POST

---
+++ server/models/todo.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose')
+
+var ToDo = mongoose.model('ToDo', {
+  text: {
+    type: String,
+    required: true,
+    minLength: 1,
+    trim: true
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  completedAt: {
+    type: Number,
+    default: null
+  }
+});
+
+module.exports = {
+  ToDo: ToDo
+};
---
+++ server/server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose.js')
+var {ToDo} = require('./models/todo.js')
+var {User} = require('./models/user.js')
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todo', (req, res) => {
+  var newToDo = new ToDo({
+    text: req.body.text
+  });
+  newToDo.save().then((result)=>{
+    res.send(result);
+  }, (err) => {
+    res.status(400).send(err);
+  });
+});
+
+
+
+
+
+
+app.listen(3000, () => {
+  console.log('Started on port 3000');
+})
