135c80a39efdee4c66e0ae8fc1a233ee3b05d91b
Lugamo <bitlugamo@gmail.com>
2018-05-26 22:03:21 -0500

    setup for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise;
-mongoose.connect(procesS.env.MONGODBURI || 'mongodb://localhost:27018/TodoApp');
+mongoose.connect(process.env.MONGODBURI || 'mongodb://localhost:27018/TodoApp');
 
 module.export = {
   mongoose: mongoose
