35dd2231125c15002bef66ef06e7ad2aa0edd456
Lugamo <bitlugamo@gmail.com>
2018-05-26 22:30:20 -0500

    Setup for heroku

--- server/server.js
+++ server/server.js
@@ -1,56 +1,56 @@
-var {ObjectID} = require('mongodb');
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
-var {mongoose} = require('./db/mongoose.js')
-var {ToDo} = require('./models/todo.js')
-var {User} = require('./models/user.js')
+var {mongoose} = require('./db/mongoose');
+var {ToDo} = require('./models/todo');
+var {User} = require('./models/user');
 
 var app = express();
 const port = process.env.PORT || 3000;
+
 app.use(bodyParser.json());
 
-app.post('/todo', (req, res) => {
-  var newToDo = new ToDo({
+app.post('/todos', (req, res) => {
+  var todo = new ToDo({
     text: req.body.text
   });
-  newToDo.save().then((result)=>{
-    res.send(result);
-  }, (err) => {
-    res.status(400).send(err);
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
   });
 });
 
-app.get('/todo', (req, res) => {
-  ToDo.find().then((todo) => {
-    res.send({todo});
-  }, (err) => {
-    res.status(400).send(err);
-  })
+app.get('/todos', (req, res) => {
+  ToDo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
 
-app.get('/todo/:id', (req, res) => {
+app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
-  if (!ObjectID.isValid(id)) {
 
-    return res.status(404).send()
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
   }
 
-  ToDo.findById(id).then((result) => {
-    if (!result) {
-      return res.status(404).send()
-    } else {
-      res.send({result});
+  ToDo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
     }
-  }, (err) => {
-    res.status(400).send()
-  })
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
 app.listen(port, () => {
-  console.log(`Started on port ${port}`);
+  console.log(`Started up at port ${port}`);
 });
 
-module.exports ={
-  app:app
-}
+module.exports = {app};
