9abd1e88cca51787677cb51abda138157fdcd06e
Lugamo <bitlugamo@gmail.com>
2018-05-26 20:54:58 -0500

    add GET /todo/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const {ObjectID} = require('mongodb');
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -25,6 +26,24 @@ app.get('/todo', (req, res) => {
     res.send({todo});
   }, (err) => {
     res.status(400).send(err);
+  })
+});
+
+app.get('/todo/:id', (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+
+    return res.send(404,{})
+  }
+
+  ToDo.findById(id).then((result) => {
+    if (!result) {
+      return res.send(404,{})
+    } else {
+      res.send('The result: \n' + JSON.stringify(result, undefined,2));
+    }
+  }, (err) => {
+    res.send(400,{})
   })
 });
 
